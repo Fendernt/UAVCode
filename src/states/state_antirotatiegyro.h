db7ec6c4449aeccfa4cf736c87ce87d2d25b62e5
Rengiebengie2 <rens.hoogendijk@gmail.com>
2025-06-11 15:10:43 +0200

    regelaarantirotatie toegevoegd en wat en include toegevoegd voor antirotati gyro

--- src/states/state_antirotatiegyro.h
+++ src/states/state_antirotatiegyro.h
@@ -1,9 +1,10 @@
-#ifndef STATE_ANTIROTATIE_H
-#define STATE_ANTIROTATIE_H
+#ifndef STATE_ANTIROTATIEGYRO_H
+#define STATE_ANTIROTATIEGYRO_H
 
 #include <Arduino.h>
 #include "../blowers/Blower.h"
 #include "../blowers/PWMTranslator.h"
+#include "../sensors/GyroSensor.h"
 
 // Externe afhankelijkheden (zoals gyro en motorfuncties)
 extern float getYawRate(); // moet elders gedefinieerd worden
